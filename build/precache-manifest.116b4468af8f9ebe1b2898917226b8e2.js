01136b3ff6f7f631f97732d66884520cb61a607d
Ladanov Nikolay <ladanov_n@mail.ru>
2020-09-14 22:58:18 +0300

    added vk hosting

---
+++ build/precache-manifest.116b4468af8f9ebe1b2898917226b8e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1e478bfd3d982095a47eac633574df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3277ea5618d870f806bc",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "3277ea5618d870f806bc",
+    "url": "/static/js/2.82a9c8b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82a9c8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5534fb052d56419998ce",
+    "url": "/static/js/main.2650bcc7.chunk.js"
+  },
+  {
+    "revision": "2f729f8dfb644b1198eb",
+    "url": "/static/js/runtime-main.4146942f.js"
+  }
+]);
